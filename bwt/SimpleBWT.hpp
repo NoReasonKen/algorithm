56b7e2e9d2809699d62a9235c3280843d3ae841f
noreason <ken5112840801@gmail.com>
2018-07-20 18:53:58 +0800

    fix some syntax error. And wanna try to handle problem of sort efficiency, so pause here for now

--- bwt/SimpleBWT.hpp
+++ bwt/SimpleBWT.hpp
@@ -4,48 +4,49 @@
 #include <string>
 #include <vector>
 
-namespace algorithm{
+namespace algorithm::bwt{
 
-class BWT
+class SimpleBWT
 {
+  protected:
     using BlockStr = std::vector<std::string>;
-    size_t block_size;
+    size_t block_size_;
 
   public:
-    BWT(size_t b_size = 256)
-	: block_size(b_size + 1)
+    SimpleBWT(size_t b_size = 256)
+	: block_size_(b_size + 1)
     {
     }
 
     std::string transform(const std::string& str)
     {
-	BlockStr bw_mat(block_size, std::string(block_size, ' '));
+	BlockStr bw_mat(block_size_, std::string(block_size_, ' '));
 	std::string out;
 
 	bw_mat[0][0] = '$';
 	for (size_t i(0); i < str.size(); i++)
 	    bw_mat[0][i + 1] = str[i];
 
-	for (size_t i(0); i < block_size - 1; i++)
+	for (size_t i(0); i < block_size_ - 1; i++)
 	    shift(bw_mat[i], bw_mat[i + 1]);
 	
 	std::sort(bw_mat.begin(), bw_mat.end());
 
-	out.resize(block_size);
-	for (size_t i(0); i < block_size; i++)
-	    out[i] = bw_mat[i][block_size - 1];
+	out.resize(block_size_);
+	for (size_t i(0); i < block_size_; i++)
+	    out[i] = bw_mat[i][block_size_ - 1];
 
 	return out;
     }
 
     std::string invert(const std::string& str)
     {
-	BlockStr bw_mat(block_size, std::string(block_size, ' '));
+	BlockStr bw_mat(block_size_, std::string(block_size_, ' '));
 
-	for (size_t i(0); i < block_size - 1; i++)
+	for (size_t i(0); i < block_size_ - 1; i++)
 	{
 	    rotate(bw_mat, i);
-	    for (size_t j(0); j < block_size; j++)
+	    for (size_t j(0); j < block_size_; j++)
 		bw_mat[j][0] = str[j];
 	    std::sort(bw_mat.begin(), bw_mat.end());
 	}
@@ -54,12 +55,12 @@ class BWT
 	return {bw_mat[0].begin() + 1, bw_mat[0].end()};
     }
 
-  private:
+  protected:
     //shift a raw to left one block
     inline void shift(const std::string& src, std::string& dest)
     {
-	for (size_t i(0); i < block_size; i++)
-	    dest[i] = src[(i + 1) % block_size];
+	for (size_t i(0); i < block_size_; i++)
+	    dest[i] = src[(i + 1) % block_size_];
     }
 
     //rotate columns whose indice less than col to right one block
