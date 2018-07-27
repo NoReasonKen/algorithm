9d3a232a5f6e8279c35e4a024fd1a51cb63c4efb
noreason <ken5112840801@gmail.com>
2018-07-27 15:17:41 +0800

    make usre results are correct, but it's poor efficiency need improved

--- sais/sais.hpp
+++ sais/sais.hpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -13,47 +12,55 @@ namespace algorithm
 
 using PairSize = std::pair<size_t, size_t>;
 using Info = std::pair<size_t, std::pair<size_t, size_t>>;
-using InfoMap = std::unordered_map<char, Info>;
-using ValMap = std::unordered_map<char, uint8_t>;
+using InfoMap = std::unordered_map<uint64_t, Info>;
+using ValMap = std::unordered_map<uint64_t, uint64_t>;
     
-std::vector<size_t> sais(const std::string&);
-void init(std::string_view, std::vector<bool>&, ValMap&, 
-	std::vector<char>&, std::vector<size_t>&, InfoMap&);
-void init_type(std::string_view, std::vector<bool>&);
-std::vector<char> init_val_map(std::string_view, ValMap&);
-void init_bucket_info(std::string_view,	const std::vector<bool>&, 
-	InfoMap&, const std::vector<char>&);
+template <typename S>
+std::vector<size_t> sais(const S&);
+template <typename S>
+void init(const S&, std::vector<bool>&, ValMap&, 
+	std::vector<uint64_t>&, std::vector<size_t>&, InfoMap&);
+template <typename S>
+void init_type(const S&, std::vector<bool>&);
+template <typename S>
+std::vector<uint64_t> init_val_map(const S&, ValMap&);
+template <typename S>
+void init_bucket_info(const S&, const std::vector<bool>&, 
+	InfoMap&, const std::vector<uint64_t>&);
 std::vector<PairSize> get_lms(const std::vector<bool>&);
-void sort_lms(std::vector<PairSize>&, std::string_view, 
+template <typename S>
+void sort_lms(std::vector<PairSize>&, const S&, 
 	const std::vector<bool>&, std::vector<size_t>&, InfoMap&, 
-	const std::vector<char>&);
-void put_bucket(const std::vector<PairSize>&, std::string_view, 
+	const std::vector<uint64_t>&);
+template <typename S>
+void put_bucket(const std::vector<PairSize>&, const S&, 
+	std::vector<size_t>&, InfoMap&, bool);
+template <typename S>
+void induced_sort(const S&, const std::vector<bool>&, 
 	std::vector<size_t>&, InfoMap&);
-void induced_sort(std::string_view, const std::vector<bool>&, 
-	std::vector<size_t>&, InfoMap&);
-std::vector<PairSize> restruct_lms(const std::vector<PairSize>&, const std::vector<bool>&, 
-	const std::vector<size_t>&);
-std::vector<size_t> update_lms(const std::vector<PairSize>&, 
-	const std::vector<size_t>&);
-std::string make_new_str(const std::vector<PairSize>&, 
-	std::string_view); 
-bool is_lms_equal(const std::vector<PairSize>&); 
+std::vector<PairSize> restruct_lms(const std::vector<PairSize>&, 
+	const std::vector<bool>&, const std::vector<size_t>&);
+template <typename S>
+std::pair<bool, std::vector<uint64_t>> make_new(
+	const std::vector<PairSize>&, const std::vector<PairSize>&, 
+	const S&); 
+template <typename S>
+bool is_lms_equal(const PairSize&, const PairSize&, const S&); 
 
-std::vector<size_t> sais(const std::string& str)
+template <typename S>
+std::vector<size_t> sais(const S& str)
 {
     clock_t c(clock());
-    //A buf for add \'$\' and then pointed by sv
-    std::string buf(str + '$');
-
-    //A string_view to replace str in function for efficiency
-    std::string_view sv(buf);
+    //A buf for add \'!\' and then pointed by sv
+    auto buf(str);
+    buf.push_back('!');
     
     //A string as long as str_, which determined each char is whether 
     //L-type(true) or S-type(false)
     std::vector<bool> type;
 
     //store every char in str, index means the rank of stored char
-    std::vector<char> val_rank;
+    std::vector<uint64_t> val_rank;
 
     //An unordered_map to map every unique char to there rank
     ValMap val_map;
@@ -68,35 +75,36 @@ std::vector<size_t> sais(const std::string& str)
     std::cout << "construct: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     c = clock();
-    init(sv, type, val_map, val_rank, bucket, bucket_info);
+    init(buf, type, val_map, val_rank, bucket, bucket_info);
     std::cout << "init: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     c = clock();
     auto v_lms(get_lms(type));
     std::cout << "get_lms: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
-    //std::cout << "num of lms: " << v_lms.size() << "\n";
+    std::cout << "num of lms: " << v_lms.size() << "\n";
 
     c = clock();
-    sort_lms(v_lms, sv, type, bucket, bucket_info, val_rank);
+    sort_lms(v_lms, buf, type, bucket, bucket_info, val_rank);
     std::cout << "sort_lms: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     c = clock();
-    put_bucket(v_lms, sv, bucket, bucket_info);
+    put_bucket(v_lms, buf, bucket, bucket_info, true);
     std::cout << "put_bucket: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     c = clock();
-    init_bucket_info(sv, type, bucket_info, val_rank);
+    init_bucket_info(buf, type, bucket_info, val_rank);
     std::cout << "init_bucket_info: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     c = clock();
-    induced_sort(sv, type, bucket, bucket_info);
+    induced_sort(buf, type, bucket, bucket_info);
     std::cout << "induced_sort: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
     return bucket;
 }
 
-void init(std::string_view sv, std::vector<bool>& type, 
-	ValMap& val_map, std::vector<char>& val_rank, 
+template <typename S>
+void init(const S& sv, std::vector<bool>& type, 
+	ValMap& val_map, std::vector<uint64_t>& val_rank, 
 	std::vector<size_t>& bucket, InfoMap& bucket_info)
 {
     type.clear();
@@ -111,7 +119,8 @@ void init(std::string_view sv, std::vector<bool>& type,
     init_bucket_info(sv, type, bucket_info, val_rank);
 }
 
-void init_type(std::string_view sv, std::vector<bool>& type)
+template <typename S>
+void init_type(const S& sv, std::vector<bool>& type)
 {
     type.resize(sv.size(), false);
 
@@ -126,9 +135,10 @@ void init_type(std::string_view sv, std::vector<bool>& type)
     }
 }
 
-std::vector<char> init_val_map(std::string_view sv, ValMap& val_map)
+template <typename S>
+std::vector<uint64_t> init_val_map(const S& sv, ValMap& val_map)
 {
-    std::vector<char> v{'$'};
+    std::vector<uint64_t> v{'!'};
     for (auto i : sv)
     {
 	size_t j(0);
@@ -147,13 +157,14 @@ std::vector<char> init_val_map(std::string_view sv, ValMap& val_map)
     return v;
 }
 
-void init_bucket_info(std::string_view sv, 
+template <typename S>
+void init_bucket_info(const S& sv, 
 	const std::vector<bool>& type, InfoMap& bucket_info, 
-	const std::vector<char>& val_rank)
+	const std::vector<uint64_t>& val_rank)
 {
     for (auto i(val_rank.begin()); i != val_rank.end(); i++)
     {
-	if (*i == '$')
+	if (*i == '!')
 	{
 	    bucket_info[*i] = {0, {0, 0}};
 	}
@@ -212,67 +223,64 @@ std::vector<PairSize> get_lms(const std::vector<bool>& type)
     return v_lms;
 }
 
-void sort_lms(std::vector<PairSize>& v_lms, std::string_view sv, 
+template <typename S>
+void sort_lms(std::vector<PairSize>& v_lms, const S& sv, 
 	const std::vector<bool>& type, std::vector<size_t>& bucket, 
-	InfoMap& bucket_info, const std::vector<char>& val_rank)
+	InfoMap& bucket_info, const std::vector<uint64_t>& val_rank)
 {
     clock_t c(clock());
-    put_bucket(v_lms, sv, bucket, bucket_info);
+    put_bucket(v_lms, sv, bucket, bucket_info, false);
     std::cout << "sort_lms-put_bucket: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
     init_bucket_info(sv, type, bucket_info, val_rank);
     c = clock();
     induced_sort(sv, type, bucket, bucket_info);
     std::cout << "sort_lms-induced_sort: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
     c = clock();
-    restruct_lms(v_lms, type, bucket);
+    auto v_lms_ordered(restruct_lms(v_lms, type, bucket));
     std::cout << "sort_lms-restruct_lms: " << (float)(clock() - c) / CLOCKS_PER_SEC << "sec\n";
 
-    /*
-    std::string new_str(make_new_str(v_lms, sv));
-    for (auto i(new_str.begin()); i != new_str.end(); i++)
-	if (*i == *(i + 1))
-	{
-	    sais(new_str);
-	    break;
-	}
-    */
+    auto new_one(make_new(v_lms, v_lms_ordered, sv));
+    if (new_one.first)
+    {
+	bucket = sais(new_one.second);
 
+	for (size_t i(0); i < v_lms_ordered.size(); i++)
+	    v_lms_ordered[i] = v_lms[bucket[i + 1]];
+    }
+
+    v_lms = std::move(v_lms_ordered);
+
+    bucket.resize(sv.size());
     for (auto& i : bucket)
-	i = sv.size() + 1;
+	i = sv.size();
     init_bucket_info(sv, type, bucket_info, val_rank);
 }
 
+template <typename S>
 void put_bucket(const std::vector<PairSize>& v_lms, 
-	std::string_view sv, std::vector<size_t>& bucket, 
-	InfoMap& bucket_info)
+	const S& sv, std::vector<size_t>& bucket, 
+	InfoMap& bucket_info, bool b)
 {
-    for (const auto& lms : v_lms)
+    if (!b)
     {
-	auto& info(bucket_info[sv[lms.first]]);
-	bucket[info.second.first] = lms.first;
-	info.second.first++;
-    }
-    /*
-    for (const auto& lms : v_lms)
-    {
-	auto& info(bucket_info[sv[lms.first]]);
-	if (is_lms)
+	for (const auto& lms : v_lms)
 	{
-	    bucket[info.second] = lms.first;
-	    if (info.first != info.second)
-		info.second--;
-	}
-	else
-	{
-	    bucket[info.first] = lms.first;
-	    if (info.first != info.second)
-		info.first++;
+	    auto& info(bucket_info[sv[lms.first]]);
+	    bucket[info.second.second--] = lms.first;
 	}
     }
-    */
+    else
+    {
+	for (const auto& lms : v_lms)
+	{
+	    auto& info(bucket_info[sv[lms.first]]);
+	    bucket[info.second.first++] = lms.first;
+	}
+    }
 }
 
-void induced_sort(std::string_view sv, const std::vector<bool>& type, 
+template <typename S>
+void induced_sort(const S& sv, const std::vector<bool>& type, 
 	std::vector<size_t>& bucket, InfoMap& bucket_info)
 {
     for (auto i(bucket.begin()); i != bucket.end(); i++)
@@ -302,7 +310,7 @@ std::vector<PairSize> restruct_lms(const std::vector<PairSize>& v_lms,
 	const std::vector<bool>& type, 
 	const std::vector<size_t>& bucket)
 {
-    std::vector<PairSize> order{std::make_pair(0, 1)};
+    std::vector<PairSize> order{std::make_pair(bucket.size() - 1, 1)};
 
     order.reserve(v_lms.size());
 
@@ -320,75 +328,56 @@ std::vector<PairSize> restruct_lms(const std::vector<PairSize>& v_lms,
     }
 
     return order;
-    /*
-    for (size_t i(0); i < bucket.size(); i++)
-	if (bucket[i] > 0 && !type[bucket[i]] && type[bucket[i] - 1])
-	    for (size_t j(idx); j < v_lms.size(); j++)
-		if (v_lms[j].first == bucket[i])
-		{
-		    auto tmp(std::move(v_lms[idx]));
-		    v_lms[idx] = std::move(v_lms[j]);
-		    v_lms[j] = tmp;
-		    idx++;
-		    if (idx == v_lms.size() - 1)
-			return;
-		}
-    */
 }
 
-std::vector<size_t> update_lms(const std::vector<PairSize>& v_lms, 
-	const std::vector<size_t>& bucket)
+template <typename S>
+std::pair<bool, std::vector<uint64_t>> make_new(
+	const std::vector<PairSize>& v_lms, 
+	const std::vector<PairSize>& v_lms_ordered, 
+	const S& sv) 
 {
-    std::vector<size_t> order;
-    
-    order.reserve(v_lms.size());
-    for (const auto& i : bucket)
-	for (const auto& j : v_lms)
-	    if (i == j.first)
-	    {
-		order.emplace_back(i);
-		
-		if (order.size() == v_lms.size())
-		    return order;
-	    }
+    std::vector<uint64_t> v;
+    bool is_dup(false);
 
-    return order;
-}
+    v.resize(v_lms.size());
 
-std::string make_new_str(const std::vector<PairSize>& v_lms, 
-	std::string_view sv) 
-{
-    std::string new_str("");
-    char alphabet('A');
-
-    new_str.reserve(v_lms.size());
-    
-    for (size_t i(0); i < v_lms.size(); i++)
+    for (size_t i(1), count(35); i < v_lms_ordered.size(); i++)
     {
-	new_str.push_back(alphabet);
+	for (size_t j(0); j < v_lms.size(); j++)
+	    if (v_lms[j].first == v_lms_ordered[i].first)
+	    {
+		if (S(sv.begin() + v_lms_ordered[i].first, 
+		    sv.begin() + v_lms_ordered[i].first + v_lms_ordered[i].second) 
+			== 
+		    S(sv.begin() + v_lms_ordered[i - 1].first, 
+		    sv.begin() + v_lms_ordered[i - 1].first + v_lms_ordered[i - 1].second
+		    ))
+		{
+		    count--;
+		    is_dup = true;
 
-	auto& lms1(v_lms[v_lms[i].first]);
-	auto& lms2(v_lms[v_lms[i + 1].first]);
-
-	if (std::string(sv.begin() + lms1.first, lms1.second) != 
-	    std::string(sv.begin() + lms2.first, lms2.second))
-	{
-	    if (alphabet == 'Z')
-		alphabet = 'a';
-	    else
-		alphabet++;
-	}
+		}
+		v[j] = count++;
+		break;
+	    }
     }
+    v.back() = 34;
 
-    return new_str;
+    return {is_dup, v};
 }
 
-bool is_lms_equal(const std::vector<PairSize>& v_lms)
+template <typename S>
+bool is_lms_equal(const PairSize& lms1, const PairSize& lms2, 
+	const S& sv)
 {
-    for (size_t i(0); i < v_lms.size() - 1; i++)
-	if (v_lms[i] == v_lms[i + 1])
-	    return true;
+	if (lms1.second == lms2.second)
+	    return false;
 
+	std::string sv1(&sv[lms1.first], lms1.second), 
+	    sv2(&sv[lms2.first], lms2.second);
+
+	if (sv1 == sv2)
+	    return true;
     return false;
 }
 
--- sais/test.cpp
+++ sais/test.cpp
@@ -1,42 +1,46 @@
 #include <gperftools/profiler.h>
-#include <iostream>
+#include <fstream>
 #include <random>
-#include <string>
-#include <vector>
 #include "sais.hpp"
 
 using algorithm::sais;
 
 int main()
 {
-    ProfilerStart("sais");
+    //ProfilerStart("sais");
     clock_t c(clock());
     std::string str;
-    //str = "immissiissippi";
-    //str = "aabaaaab";
-    //str = "mmississiippii";
-    ;
+    
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> uid1(0, 25);
     std::uniform_int_distribution<> uid2(0, 3);
-
-    str.reserve(30000000);
+        
+    str.reserve(100000);
     std::unordered_map<int, char> m{{0, 'A'}, {1, 'C'}, {2, 'G'}, {3, 'T'}};
-    for (size_t i(0); i < 30000000; i++)
+    for (size_t i(0); i < 100000; i++)
 	str.push_back(m[uid2(gen)]);
-
+    
     c = clock() - c;
     std::cout << "Generate str " << (float)c / CLOCKS_PER_SEC << "sec\n";
     //std::cout << str << "\n";
     std::cout << "SA-IS algorithm start:\n";
 
     c = clock();
+    //std::ifstream ifs("str");
+    //std::getline(ifs, str);
     auto v(sais(str));
+    std::ofstream ofstr("str");
+    std::ofstream ofs("my_out");
+    ofstr << str << "\n";
+    for (size_t i(1); i < v.size(); i++)
+	ofs << v[i] << " ";
+    ofs << "\n";
     c = clock() - c;
     std::cout << "Total: " << (float)c / CLOCKS_PER_SEC << "sec\n";
 
-    ProfilerStop();
+    //ProfilerStop();
+    
     /*
     for (const auto& i : v)
 	std::cout << i << " ";
